2314c897cdf4f927f4dee433194c245d30f92b0d
Dovydas Rupsys <dovydas.rupsys@mvfglobal.com>
2022-02-16 08:02:16 +0000

    Add additional Expire constants

--- constants/Expire.php
+++ constants/Expire.php
@@ -6,7 +6,9 @@ namespace OneDark\TombstoneRedis\Constants;
 
 class Expire
 {
-    const ONE_DAY = 86400;
+    const ONE_HOUR = 3600;
+    const TWO_HOURS = self::ONE_HOUR * 2;
+    const ONE_DAY = self::ONE_HOUR * 24;
     const TWO_DAYS = self::ONE_DAY * 2;
     const ONE_WEEK = self::ONE_DAY * 7;
     const FOUR_WEEKS = self::ONE_WEEK * 4;
